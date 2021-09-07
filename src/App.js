b777634066c4ff9823f8f6c91f30fc0bd5822a8c
HaVanThuong <thuongrevolt2@gmail.com>
2021-09-07 16:00:52 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>My react app</h1>
+        <h2>Thuonghv</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
